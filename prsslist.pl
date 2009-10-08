ef4d6c8f6aab6706218deb781cd46423b9ff2565
lantrix <lantrix@pobox.com>
2009-10-08 17:42:13 +1100

    Updated licence and copyright notices for pRSSlist main files. Still under GPLv2.

--- prsslist.pl
+++ prsslist.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
 
 #
-# (c) Copyright 2004, 2005 Software Garden, Inc.
-# All Rights Reserved.
+# Original software (c) Copyright 2004, 2005 Software Garden, Inc. All Rights Reserved.
+# pRSSlist Copyright (c) 2009, lantrix, TechDebug.com
 # Subject to Software License at the end of this file
 #
 
@@ -204,9 +204,8 @@ See Software License in the program file.
 
 SOFTWARE LICENSE
 
-This software and documentation is
-Copyright (c) 2004, 2005 Software Garden, Inc.
-All rights reserved. 
+Original software (c) Copyright 2004, 2005 Software Garden, Inc. All Rights Reserved.
+pRSSlist Copyright (c) 2009, lantrix, TechDebug.com
 
 1. The source code of this program is made available as free software;
 you can redistribute it and/or modify it under the terms of the GNU
@@ -221,63 +220,26 @@ copy of the GNU General Public License along with this program; if not,
 write to the Free Software Foundation, Inc., 59 Temple Place - Suite
 330, Boston, MA  02111-1307, USA.
 
-3. If the GNU General Public License is restrictive in a way that does
-not meet your needs, contact the copyright holder (Software Garden,
-Inc.) to inquire about the availability of other licenses, such as
-traditional commercial licenses. 
-
-4. The right to distribute this software or to use it for any purpose
-does not give you the right to use Servicemarks or Trademarks of
-Software Garden, Inc., including Garden, Software Garden, and
-ListGarden. 
-
-5. An appropriate copyright notice will include the Software Garden,
-Inc., copyright, and a prominent change notice will include a
+3. An appropriate copyright notice will include the original Software Garden,
+Inc. copyright plus the pRSSlist copyright, and a prominent change notice will include a
 reference to Software Garden, Inc., as the originator of the code
 to which the changes were made.
 
-Exception for Executable Bundle 
-
-In some cases this program is distributed together with programs and
-libraries of ActiveState Corporation as a single executable file (an
-"Executable Bundle") produced using ActiveState Corporation's "Perl Dev
-Kit" PerlTray program ("PDK PerlTray"). This free software license does
-not apply to those programs and libraries of ActiveState Corporation
-that are part of the Executable Bundle. You only have a license to use
-those programs and libraries of ActiveState Corporation for runtime
-purposes in order to execute this software of Software Garden, Inc. In
-order to create and distribute similar executable files from modified
-source files, you will need to license your own copy of PDK PerlTray. 
-
-As a specific exception for this product to the terms and conditions of
-the GNU General Public License version 2, you are free to distribute
-this software (modified or unmodified) in an Executable Bundle created
-with PDK PerlTray as long as you adhere to the GNU General Public
-License in all respects for all software components except for those of
-PDK PerlTray added by that program when used to create the Executable
-Bundle. 
-
 Disclaimer 
 
-THIS SOFTWARE IS PROVIDED BY SOFTWARE GARDEN, INC., "AS IS" AND ANY
-EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-WARRANTIES OF INFRINGEMENT AND THE IMPLIED WARRANTIES OF
-MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-IN NO EVENT SHALL SOFTWARE GARDEN, INC. NOR ITS EMPLOYEES AND OFFICERS
-BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-OTHERWISE) ARISING IN ANY WAY OUT OF THE DISTRIBUTION OR USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+THE SOFTWARE IS PROVIDED “AS IS” AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
+Original Software Creator:
 Software Garden, Inc.
 PO Box 610369
 Newton Highlands, MA 02461 USA
 www.softwaregarden.com
-
-License version: 1.2/2005-07-27 
 
 =end
 
